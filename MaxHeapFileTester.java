7ba2293e289d2a6ce3c9ddc5e5dca31706762c37
Manji <Marco.w246@gmail.com>
2021-11-06 18:40:30 -0700

    adding on to the test

--- MaxHeapFileTester.java
+++ MaxHeapFileTester.java
@@ -18,5 +18,20 @@ public class MaxHeapFileTester {
 		}
 
 		MaxHeap.buildMaxHeap_optimal(random);
+		
+		System.out.println();
+		
+		File sortedData = new File("C:\\Users\\Justin\\OneDrive - Cal Poly Pomona\\Progamming stuff\\CS2400_Project_4\\data_sorted.txt");
+		Scanner reader2 = new Scanner(sortedData);
+		String[] sorted = new String[100];
+		
+		i = 0;
+		while (reader1.hasNextLine())
+		{
+			sorted[i] = reader2.nextLine();
+			++i;
+		}
+		
+		MaxHeap.buildMaxHeap_optimal(sorted);
 	}
 }
