99e806fd8af5a457f4e631b5ee27c6fc611615ba
Sulfurous-Impersonation <june@watson-net.com>
2021-11-07 18:46:10 -0800

    tester now writes to data_sorted.txt

--- MaxHeapFileTester.java
+++ MaxHeapFileTester.java
@@ -1,14 +1,17 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MaxHeapFileTester {
 
 	public static void main(String[] args) throws FileNotFoundException
 	{
-		File randomData = new File("C:\\Users\\Justin\\OneDrive - Cal Poly Pomona\\Progamming stuff\\CS2400_Project_4\\data_random.txt");
+		File randomData = new File("data_random.txt");
 		Scanner reader1 = new Scanner(randomData);
-		String[] random = new String[100];
+		Comparable[] random = new String[100];
 		
 		int i = 0;
 		while (reader1.hasNextLine())
@@ -17,21 +20,43 @@ public class MaxHeapFileTester {
 			++i;
 		}
 
-		MaxHeap.buildMaxHeap_optimal(random);
+
+		//optimal method
+		System.out.println(Arrays.toString(random) + "\n");
+		int swaps = MaxHeap.buildMaxHeap_optimal(random);
+		System.out.println("\n" + Arrays.toString(random));
 		
 		System.out.println();
 		
-		File sortedData = new File("C:\\Users\\Justin\\OneDrive - Cal Poly Pomona\\Progamming stuff\\CS2400_Project_4\\data_sorted.txt");
-		Scanner reader2 = new Scanner(sortedData);
-		String[] sorted = new String[100];
-		
-		i = 0;
-		while (reader1.hasNextLine())
-		{
-			sorted[i] = reader2.nextLine();
-			++i;
-		}
-		
-		MaxHeap.buildMaxHeap_optimal(sorted);
+		//File sortedData = new File("data_sorted.txt");
+		//Scanner reader2 = new Scanner(sortedData);
+		Comparable[] sorted = new String[100];
+		try {
+			FileWriter writer = new FileWriter("data_sorted.txt");
+
+			//i = 0;
+			MaxHeap heap = new MaxHeap(random);
+
+			for (int j = 0; j < 10; j++)
+			{
+				//sorted[i] = reader2.nextLine();
+				writer.write( (j + 1) + ": " + random[j] + "\n"); //write first 10 ints to output
+				heap.removeMax(); //perform 10 removals
+			}
+			writer.write("Number of swaps: " + swaps + "\n\n");
+			
+			
+			for (int j = 0; j < 10; j++)
+			{
+				//write to output heap after 10 removals
+				sorted[j] = heap.removeMax();
+				
+				writer.write( (j + 1) + ": " + sorted[j] + "\n");
+			}
+			writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
 	}
 }
